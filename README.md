7eceecfbef4174fbda77433d4ecb1342bd8fe1c5
Schell Carl Scivally <efsubenovex@gmail.com>
2024-02-10 10:22:10 +1300

    compile times are blowing up by level 7

---
+++ README.md
@@ -0,0 +1,12 @@
+```
+cd compile
+cargo run --release -- --no-default-features --feature baseline
+cargo run --release -- --no-default-features --feature level0
+cargo run --release -- --no-default-features --feature level1
+cargo run --release -- --no-default-features --feature level2
+cargo run --release -- --no-default-features --feature level3
+cargo run --release -- --no-default-features --feature level4
+cargo run --release -- --no-default-features --feature level5
+cargo run --release -- --no-default-features --feature level6
+cargo run --release -- --no-default-features --feature level7
+```
--- example/src/lib.rs
+++ example/src/lib.rs
@@ -2,6 +2,13 @@
 use crabslab::{Id, Slab, SlabItem};
 use spirv_std::spirv;
 
+#[cfg(feature = "baseline")]
+#[spirv(fragment)]
+pub fn fragment(color: [f32; 4], frag_color: &mut [f32; 4]) {
+    *frag_color = color;
+}
+
+#[cfg(feature = "level0")]
 #[derive(Default, Clone, Copy, SlabItem)]
 pub struct Level0([f32; 4]);
 
@@ -16,6 +23,7 @@ pub fn fragment0(
     *frag_color = val.0;
 }
 
+#[cfg(feature = "level1")]
 #[derive(Default, Clone, Copy, SlabItem)]
 pub struct Level1(Level0, Level0);
 
@@ -30,6 +38,7 @@ pub fn fragment1(
     *frag_color = val.0 .0;
 }
 
+#[cfg(feature = "level2")]
 #[derive(Default, Clone, Copy, SlabItem)]
 pub struct Level2(Level1, Level1);
 
@@ -44,6 +53,7 @@ pub fn fragment2(
     *frag_color = val.0 .0 .0;
 }
 
+#[cfg(feature = "level3")]
 #[derive(Default, Clone, Copy, SlabItem)]
 pub struct Level3(Level2, Level2);
 
@@ -58,6 +68,7 @@ pub fn fragment3(
     *frag_color = val.0 .0 .0 .0;
 }
 
+#[cfg(feature = "level4")]
 #[derive(Default, Clone, Copy, SlabItem)]
 pub struct Level4(Level3, Level3);
 
@@ -72,6 +83,7 @@ pub fn fragment4(
     *frag_color = val.0 .0 .0 .0 .0;
 }
 
+#[cfg(feature = "level5")]
 #[derive(Default, Clone, Copy, SlabItem)]
 pub struct Level5(Level4, Level4);
 
@@ -84,4 +96,34 @@ pub fn fragment5(
 ) {
     let val = slab.read(id);
     *frag_color = val.0 .0 .0 .0 .0 .0;
+}
+
+#[cfg(feature = "level6")]
+#[derive(Default, Clone, Copy, SlabItem)]
+pub struct Level6(Level5, Level5);
+
+#[cfg(feature = "level6")]
+#[spirv(fragment)]
+pub fn fragment6(
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] slab: &[u32],
+    #[spirv(flat)] id: Id<Level6>,
+    frag_color: &mut [f32; 4],
+) {
+    let val = slab.read(id);
+    *frag_color = val.0 .0 .0 .0 .0 .0 .0;
+}
+
+#[cfg(feature = "level7")]
+#[derive(Default, Clone, Copy, SlabItem)]
+pub struct Level7(Level6, Level6);
+
+#[cfg(feature = "level7")]
+#[spirv(fragment)]
+pub fn fragment7(
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] slab: &[u32],
+    #[spirv(flat)] id: Id<Level7>,
+    frag_color: &mut [f32; 4],
+) {
+    let val = slab.read(id);
+    *frag_color = val.0 .0 .0 .0 .0 .0 .0 .0;
 }
